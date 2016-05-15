cf152fc09adf2cbd82e70d903c87674fbf16dc53
Armaan Shah <armaanshah96@gmail.com>
2016-05-15 12:14:07 -0500

    2nd page edits

--- buggy.js
+++
@@ -1,7 +0,0 @@
-function validateForm() {
-    var x = document.forms["firstInput"]["date"].value;
-    if (x == null || x == "") {
-        alert("Airport and Date must be selected");
-        return false;
-    }
-}
